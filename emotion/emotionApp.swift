8be44cc420edfc14cec5e63b495457edf5bfab28
sonerxdev <sonerkaraevli@gmail.com>
2023-02-06 22:39:36 +0300

    Initial Commit

---
+++ emotion/emotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  emotionApp.swift
+//  emotion
+//
+//  Created by Soner Karaevli on 6.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct emotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
